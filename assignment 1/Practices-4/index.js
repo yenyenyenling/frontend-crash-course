315170016b2d988a8705ff46675487ee4b0b5e05
yenyenyenling <118142093+yenyenyenling@users.noreply.github.com>
2023-05-07 20:47:29 +0800

    Update index.js

--- assignment 1/Practices-4/index.js
+++ assignment 1/Practices-4/index.js
@@ -2,9 +2,9 @@
 // 1.
 // 1~10000
 // for
-// for (let num1 = 1; num1 <= 10000; num1++) {
-//     console.log(num1);
-// }
+for (let num1 = 1; num1 <= 10000; num1++) {
+    console.log(num1);
+}
 // while
 // let num1 = 1;
 // while (num1 <= 10000) {
@@ -15,12 +15,12 @@
 // 2.
 // 1~100 even
 // for
-// for (let num2 = 1; num2 <= 100; num2++) {
-//     if (num2 % 2 == 0) {
-//         console.log(num2);
-//     }
-//     num2++;
-// }
+for (let num2 = 1; num2 <= 100; num2++) {
+    if (num2 % 2 == 0) {
+        console.log(num2);
+    }
+    num2++;
+}
 // while
 // let num2 = 1;
 // while (num2 <= 100) {
@@ -33,11 +33,11 @@
 // 3.
 // 1~100 odd
 // for
-// for (let num3 = 1; num3 <= 100; num3++) {
-//     if (num3 % 2 != 0) {
-//         console.log(num3);
-//     }
-// }
+for (let num3 = 1; num3 <= 100; num3++) {
+    if (num3 % 2 != 0) {
+        console.log(num3);
+    }
+}
 // while
 // let num3 = 1;
 // while (num3 <= 100) {
@@ -84,20 +84,20 @@ for (let i = 2; i <= 100; i++) {
 // 5.
 // size大小的三角形
 // for
-// let count = 7;
-// let star = '';
-// for (let i = 1; i <= count; i++) {
-//     for (let j = 1; j <= i; j++) {
-//         star += '*';
-//     }
-//     star += '\n';
-// }
-// console.log(star);
+let count = 7;
+let star = '';
+for (let i = 1; i <= count; i++) {
+    for (let j = 1; j <= i; j++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log(star);
 
-// while
-// let count = 3;
-// let star = '';
-// let i = 1;
+while
+let count = 3;
+let star = '';
+let i = 1;
 
 // while (i <= count) {
 //     let j = 1;
@@ -113,16 +113,16 @@ for (let i = 2; i <= 100; i++) {
 // 6.
 // 99乘法表
 // for
-// let table = 5;
-// for (let row = 1; row <= table; row++) {
-//     let ans = '';
-//     for (let col = 1; col <= table; col++) {
-//         let x = row * col;
-//         // ans += x + '\t';  =>   ans = ans + (x + '\t');
-//         ans += x + '\t';
-//     }
-//     console.log(ans);
-// }
+let table = 9;
+for (let row = 1; row <= table; row++) {
+    let ans = '';
+    for (let col = 1; col <= table; col++) {
+        let x = row * col;
+        // ans += x + '\t';  =>   ans = ans + (x + '\t');
+        ans += x + '\t';
+    }
+    console.log(ans);
+}
 
 // while
 // let table = 9;
